df486ce7d08f0c0c4f384355f5c41b1fc085544b
bo-lim <leebolim0701@cau.ac.kr>
2023-05-29 19:06:10 +0900

    [FEATURE] 비속어 필터링 기능 추가

---
+++ 비속어필터링.py
@@ -0,0 +1,22 @@
+def get_data(filename):
+    with open("./DB/" + filename) as f:
+        lines = f.readlines()
+    data = []
+    for d in lines:
+        data.append(d.strip())
+    data.sort(key= lambda x : len(x))
+    return data
+
+ko_data = get_data("KO")
+
+def bad2star(line):
+    used_badword = []
+    for badword in ko_data:
+        if badword in line:
+            line = line.replace(badword,'*'*len(badword))
+            used_badword.append(badword)
+    return (line,used_badword)
+
+result, used_badword = bad2star('이제 그만 닥쳐봐')
+print(result)
+print(used_badword)
